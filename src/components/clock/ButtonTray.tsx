714437009173e9fa2412036da1b15c75951dd4ed
Phil Marshall <phillipsolomonmarshall@gmail.com>
2019-08-27 22:31:46 -0500

    refactor button tray into functional component

--- src/components/clock/ButtonTray.tsx
+++ src/components/clock/ButtonTray.tsx
@@ -1,8 +1,8 @@
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core";
 import PauseIcon from "@material-ui/icons/Pause";
 import RefreshIcon from "@material-ui/icons/Refresh";
 import SettingsIcon from "@material-ui/icons/Settings";
-import React, { PureComponent } from "react";
+import React from "react";
 import { GameLifecycle } from "../../types";
 
 // declare let navigator: any;
@@ -22,43 +22,41 @@ const styles = {
   },
 };
 
-class ButtonTray extends PureComponent<IProps> {
-  public render() {
-    const {
-      classes,
-      gameLifecycle,
-      onClickSettingsButton,
-      onClickPauseButton,
-      onClickResetButton,
-    } = this.props;
-    return (
-      <React.Fragment>
-        <div className="topButton">
-          <SettingsIcon
+const ButtonTray = (props: IProps) => {
+  const {
+    classes,
+    gameLifecycle,
+    onClickSettingsButton,
+    onClickPauseButton,
+    onClickResetButton,
+  } = props;
+  return (
+    <React.Fragment>
+      <div className="topButton">
+        <SettingsIcon
+          classes={{ root: classes.root }}
+          fontSize="large"
+          onClick={onClickSettingsButton}
+        />
+      </div>
+      {gameLifecycle === GameLifecycle.InProgress && (
+        <div className="middleButton">
+          <PauseIcon
             classes={{ root: classes.root }}
+            onClick={onClickPauseButton}
             fontSize="large"
-            onClick={onClickSettingsButton}
           />
         </div>
-        {gameLifecycle === GameLifecycle.InProgress && (
-          <div className="middleButton">
-            <PauseIcon
-              classes={{ root: classes.root }}
-              onClick={onClickPauseButton}
-              fontSize="large"
-            />
-          </div>
-        )}
-        <div className="bottomButton">
-          <RefreshIcon
-            classes={{ root: classes.root }}
-            fontSize="large"
-            onClick={onClickResetButton}
-          />
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+      )}
+      <div className="bottomButton">
+        <RefreshIcon
+          classes={{ root: classes.root }}
+          fontSize="large"
+          onClick={onClickResetButton}
+        />
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default withStyles(styles)(ButtonTray);
